3939075a42110c0d616ee52102799f631b203386
Anton Zimin <azimin1@saddleback.edu>
2025-08-05 00:56:46 -0700

    Load summary and questions

--- app/controllers/summary_controller.py
+++ app/controllers/summary_controller.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from services.summary_service import SummaryService
+import json
 
 router = APIRouter(prefix="/api/summary", tags=["summary"])
 
 @router.get("/{id}")
 async def get_summary(id: int):
     summary_service = SummaryService()
-    return summary_service.get_summary()
+    summary_string = summary_service.get_summary()
+    summary = json.loads(summary_string)
+    return summary
