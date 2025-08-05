af0946025817cf5f0506d1267ecceb497d900729
Anton Zimin <azimin1@saddleback.edu>
2025-08-05 01:23:47 -0700

    Implement the Summarize button

--- app/controllers/summary_controller.py
+++ app/controllers/summary_controller.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 from services.summary_service import SummaryService
 import json
 
 router = APIRouter(prefix="/api/summary", tags=["summary"])
 
-@router.get("/{id}")
+@router.get("/full/{id}", response_class = PlainTextResponse)
+async def get_full(id: int):
+    summary_service = SummaryService()
+    return summary_service.get_full()
+
+@router.get("/short/{id}")
 async def get_summary(id: int):
     summary_service = SummaryService()
     summary_string = summary_service.get_summary()
--- app/services/summary_service.py
+++ app/services/summary_service.py
@@ -24,3 +24,8 @@ class SummaryService:
         prompt = self._summary_prompt.format(textbook_content = content)
 
         return lang_chain_service.invoke_model(prompt)
+
+    def get_full(self):
+        with open('content/24.1_says_law_and_the_macroeconomics_of_supply.txt', 'r', encoding='utf-8') as file:
+            content = file.read()
+            return content
