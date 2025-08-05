40facfacc1a3422853e355f3c32c33718e365904
Anton Zimin <azimin1@saddleback.edu>
2025-08-05 07:34:23 -0700

    Add 404 error

--- app/services/content_service.py
+++ app/services/content_service.py
@@ -1,11 +1,13 @@
 import os
 
+from fastapi import HTTPException
+
 class ContentService:
     def read_section(self, section_id):
         file_name = 'content/' + str(section_id) + '.txt'
 
         if not os.path.exists(file_name):
-            return None
+            raise HTTPException(status_code=404, detail="Item not found")
 
         with open(file_name, 'r', encoding='utf-8') as file:
             content = file.read()
