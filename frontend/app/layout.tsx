d8020789a71274490b6ff38bc271ea79071b624c
Anton Zimin <azimin1@saddleback.edu>
2025-08-07 14:50:04 -0700

    Add favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
